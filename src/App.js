cbdc8ae7fb4f629e384cf4d8db2e27f9cfe4977c
User <user@example.com>
2022-06-30 20:46:04 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Center } from "@mantine/core";
+import { ToastContainer, Zoom } from "react-toastify";
+
+import Quote from "./Quote";
 
 function App() {
+  const [quote, setQuote] = useState(null);
+
+  const getQuote = () => {
+    axios.get('https://api.quotable.io/random')
+      .then((res) => {
+        setQuote(res.data);
+      }).catch((err) => {
+        alert(err);
+      })
+  }
+
+  useEffect(() => {
+    getQuote();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: "#22b8cf" }}>
+      <ToastContainer transition={Zoom} autoClose={3000} hideProgressBar={true} icon={false} />
+      <Container>
+        <Center style={{ height: "100vh" }}>
+          <Quote author={quote ? quote.author : ""} content={quote ? quote.content : ""} getQuote={getQuote} />
+        </Center>
+      </Container>
+
     </div>
   );
 }
---
+++ src/Quote.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Volume, Copy, BrandTwitter } from 'tabler-icons-react';
+import { Paper, Blockquote, Button, Box, Divider, Space, ActionIcon } from "@mantine/core"
+import { toast } from 'react-toastify';
+
+const synth = window.speechSynthesis;
+
+const speak = (content) => {
+    if (synth.speaking) {
+        return;
+    }
+    const toSpeak = new SpeechSynthesisUtterance(content);
+    toSpeak.onend = e => {
+        console.log('Done speaking...');
+    }
+    toSpeak.onerror = e => {
+        console.log("Error on speaking...");
+    }
+    synth.speak(toSpeak);
+}
+
+export default function Quote(props) {
+    const { content, author, getQuote } = props;
+
+    if (!content) {
+        return null;
+    }
+
+    const handleSpeakOnClick = () => {
+        speak(content);
+    }
+
+    const handleCopyQuote = () => {
+        navigator.clipboard.writeText(content);
+        toast.info("Quote Copied");
+    }
+
+    return (
+        <Paper shadow="xs" p="md" style={{ minWidth: "80vw" }}>
+            <Blockquote cite={author ? `- ${author}` : ""}>
+                {content}
+            </Blockquote>
+            <Space h="md" />
+            <Divider />
+            <Space h="md" />
+            <Box sx={{
+                display: "flex",
+                justifyContent: "space-between"
+            }}>
+                <Box sx={{ display: "flex" }}>
+                    <ActionIcon size="lg" variant="outline" onClick={handleSpeakOnClick}>
+                        <Volume size={24} />
+                    </ActionIcon>
+                    <Space w="xs" />
+                    <ActionIcon onClick={handleCopyQuote} size="lg" variant="outline">
+                        <Copy size={24} />
+                    </ActionIcon>
+                    <Space w="xs" />
+                    <ActionIcon component="a" target="_blank" href={`https://twitter.com/intent/tweet?text=${content} - ${author}`} size="lg" variant="outline">
+                        <BrandTwitter size={24} />
+                    </ActionIcon>
+                </Box>
+                <Button onClick={getQuote}>New Quote</Button>
+            </Box>
+        </Paper>
+    )
+}
